a90e83a3dee33bdca12644b60675ee2a012a6d21
Brylie Oxley <brylie@gnumedia.org>
2013-08-26 11:20:40 +0300

    Allow for multiple dice.

--- manyDiceRolls.R
+++ manyDiceRolls.R
@@ -3,14 +3,33 @@
 # Adapted from Jonathan Stray lecture
 # https://www.youtube.com/watch?v=3NuyRKNkBQg
 
-roll <- function() {
-  sample(1:6,1)
+rollNDice <- function(numberOfDice) {
+  # Roll a number of dice
+  # return summed dice rolls
+  
+  # Initialize rolls counter
+  rolls <- 0
+  
+  for (i in 1:numberOfDice) {
+    roll <- sample(1:6,1)
+    rolls <- rolls + roll
+  }
+  return(rolls)
 }
-manyrolls <- function(n) {
+manyrolls <- function(numberOfRolls,numberOfDice) {
+  # Roll a number of dice a number of times
   par(mfrow = c(3,3))
+  
+  # Use this to set the minimum histogram display value
+  # Not currently in use
+  minHist = numberOfRolls
+  
+  # Maximum number that the histogram will display
+  # i.e. if all rolls are six
+  maxHist = (numberOfDice * 6) + 1
   for (i in 1:9) {
-    hist(replicate(n,dieroll())
-         ,breaks=seq(1:7)
+    hist(replicate(numberOfRolls,rollNDice(numberOfDice))
+         ,breaks=seq(1:maxHist)
          ,right=FALSE
          ,xlab=NULL
          ,ylab=NULL
